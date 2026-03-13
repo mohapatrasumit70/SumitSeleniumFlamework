08d0c1a757ae52fb7bf834f06be578e201ab620e
sumit <mohapatrasumit70@gmail.com>
2026-03-13 17:30:00 +0530

    Push To Sumit

--- OwnSumitSeleniumFlamewrork/src/test/java/test/HomePageTest.java
+++ OwnSumitSeleniumFlamewrork/src/test/java/test/HomePageTest.java
@@ -14,25 +14,25 @@ import utilities.ExtentManager;
 
 public class HomePageTest extends BaseClass {
 	
-//	private LoginPage loginPage;
-//	private HomePage homePage;
-//
-//	@BeforeMethod
-//	public void setupPages() {
-//		loginPage = new LoginPage(getDriver());
-//		homePage  = new HomePage(getDriver());
-//	}
-//
-//	@Test(dataProvider="validLoginData", dataProviderClass = DataProviders.class)
-//	public void verifyOrangeHRMLogo(String username, String password) {
-//		//ExtentManager.startTest("Home Page Verify Logo Test"); --This has been implemented in TestListener
-//		ExtentManager.logStep("Navigating to Login Page entering username and password");
-//		loginPage.login(username, password);
-//		ExtentManager.logStep("Verifying Logo is visible or not");
-//		Assert.assertTrue(homePage.verifyOrangeHRMlogo(),"Logo is not visible");
-//		ExtentManager.logStep("Validation Successful");
-//		homePage.logout();
-//		ExtentManager.logStep("Logged out Successfully!");
-//	}
+	private LoginPage loginPage;
+	private HomePage homePage;
+
+	@BeforeMethod
+	public void setupPages() {
+		loginPage = new LoginPage(getDriver());
+		homePage  = new HomePage(getDriver());
+	}
+
+	@Test(dataProvider="validLoginData", dataProviderClass = DataProviders.class)
+	public void verifyOrangeHRMLogo(String username, String password) {
+		//ExtentManager.startTest("Home Page Verify Logo Test"); --This has been implemented in TestListener
+		ExtentManager.logStep("Navigating to Login Page entering username and password");
+		loginPage.login(username, password);
+		ExtentManager.logStep("Verifying Logo is visible or not");
+		Assert.assertTrue(homePage.verifyOrangeHRMlogo(),"Logo is not visible");
+		ExtentManager.logStep("Validation Successful");
+		homePage.logout();
+		ExtentManager.logStep("Logged out Successfully!");
+	}
 
 }
